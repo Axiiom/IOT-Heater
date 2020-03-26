a039b7c99aed8feb339a271a4855357840933607
axiiom <camallanyoung@gmail.com>
2020-03-26 16:28:56 -0400

    implemented queues and continuous temperature sampling

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from flask import Flask, jsonify, request
+import threading, queue, time
+
 import routes
+
+
+def get_temperature():
+    pass
+
+
+# global temperature sampling thread
+def continuous_sample(q):
+    while True:
+        temperature = get_temperature()
+        if not q.empty():
+            target, deadzone = q.get()
+
+        if int(temperature) not in range( target-deadzone, target+deadzone ):
+            # toggle the thing
+            pass
+
+        time.sleep(5)
 
 # global state
 gState = {
@@ -12,8 +32,19 @@ gState = {
     }
 }
 
+# setup app
 app = Flask(__name__)
 
+# setup temperature sampler
+q = queue.Queue()
+with open("state.json") as file:
+    file = json.loads(file)
+    q.put(( file["climate"]["target"], file["climate"]["deadzone"] ))
+
+sampler = threading.Thread(target=continuous_sample, args=(q, ))
+sampler.start()
+
+# routes
 @app.route("/api/state", methods=["GET"])
 def get_state():
     return jsonify(routes.get_state(gState))
@@ -22,6 +53,7 @@ def get_state():
 def set_state():
     global gState
     gState, resp = routes.set_state(request, gState)
+    q.put( (gState["climate"]["target"], gState["climate"]["deadzone"]) )
     return jsonify(resp)
 
 @app.route("/api/state/climate")
@@ -32,6 +64,6 @@ def get_climate():
 def get_history():
     return jsonify(routes.get_history(request))
 
-@app.route("/api/history/temperature")
-def get_temperature():
-    return jsonify(routes.get_history_temperature(request))
+@app.route("/api/history/climate")
+def get_history_climate():
+    return jsonify(routes.get_history_climate(request))
