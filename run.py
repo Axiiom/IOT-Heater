34b68530f1b4e070187eec279cef7b38776e0f15
axiiom <camallanyoung@gmail.com>
2020-03-30 14:35:18 -0400

    commented out flask_sqlalchemy db - uneeded for now

--- run.py
+++ run.py
@@ -13,15 +13,15 @@ Session = scoped_session(sessionmaker(bind=engine))
 
 if(__name__ == '__main__'):
     app.config.from_object(Config)
-    db = SQLAlchemy(app)
+    # db = SQLAlchemy(app)
 
     print("Starting temperature sampling thread ... ")
     sampler.start()
     print("Done")
 
-    print("Initializing database ... ")
-    db.create_all()
-    print("Done")
+    # print("Initializing database ... ")
+    # db.create_all()
+    # print("Done")
 
     print("Starting application")
     app.run(host="0.0.0.0", port=5000, debug=True)
